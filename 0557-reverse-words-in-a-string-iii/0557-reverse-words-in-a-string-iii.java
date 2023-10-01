333f3e4f7f334835609eb193c76a0ef125012063
Shobhit Gupta <96045678+shobhit0888@users.noreply.github.com>
2023-10-02 01:04:53 +0530

    Time: 34 ms (14.49%), Space: 44.3 MB (51.46%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,19 +1,19 @@
+import java.util.*;
 class Solution {
     public String reverseWords(String s) {
-         String str="";
-         String ss="";
-       for(int i=0;i<s.length();i++){
-           char ch=s.charAt(i);
-           if(Character.isWhitespace(ch)){
-             str=str+ss+" ";
-             ss="";
-           }
-           else{
-               ss=ch+ss;
-           }
-       }
-       str=str+ss;
-       return str;
-    
+        char[] ch = s.toCharArray();
+        String str="";
+        String ss="";
+        for(int i=0;i<ch.length;i++){
+          if(ch[i]==' '){
+               str=str+ss+" ";
+               ss="";
+          }
+          else{
+            ss=ch[i]+ss;
+          }
+        }
+        str=str+ss;
+        return str;
     }
 }
