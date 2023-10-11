e9da8fb2c625f1661b81f8540a3293957c08dd92
Shobhit Gupta <96045678+shobhit0888@users.noreply.github.com>
2023-10-11 23:36:13 +0530

    Time: 35 ms (100%), Space: 55.9 MB (89.32%) - LeetHub

--- 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.java
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.java
@@ -1,21 +1,28 @@
 class Solution {
     public int minOperations(int[] nums) {
-        Arrays.sort(nums);
-        int uniqueLen = 1;
-        for (int i = 1; i < nums.length; ++i) {
+        int n = nums.length;
+        int ans = n; // Initialize the answer to the length of the input array
+        Arrays.sort(nums); // Sort the input array in ascending order
+        int m = 1; // Initialize a variable to keep track of unique elements count
+        int j = 0;
+
+        // Loop to find unique elements and count them
+        for (int i = 1; i < n; i++) {
             if (nums[i] != nums[i - 1]) {
-                nums[uniqueLen++] = nums[i];
+                nums[m] = nums[i]; // Store unique elements at the beginning of the array
+                m++; // Increment the count of unique elements
             }
         }
-        
-        int ans = nums.length;
-        for (int i = 0, j = 0; i < uniqueLen; ++i) {
-            while (j < uniqueLen && nums[j] - nums[i] <= nums.length - 1) {
-                ++j;
+
+        // Iterate through unique elements to calculate the minimum operations
+        for (int i = 0; i < m; i++) {
+            while (j < m && nums[j] < nums[i] + n) {
+                j++; // Move the 'j' pointer until the condition is met
             }
-            ans = Math.min(ans, nums.length - (j - i));
+            // Calculate and update the minimum operations
+            ans = Math.min(ans, n - j + i);
         }
-        
-        return ans;
+
+        return ans; // Return the minimum operations required
     }
 }
