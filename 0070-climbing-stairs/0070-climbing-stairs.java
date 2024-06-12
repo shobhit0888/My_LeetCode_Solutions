c15da38971e160d75df25acfd7c577db192f43ad
Shobhit Gupta <96045678+shobhit0888@users.noreply.github.com>
2024-06-12 11:52:22 +0530

    Time: 0 ms (100%), Space: 40 MB (70.27%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+    int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return fibo(n,dp); 
+    }
+    int fibo (int n, int[] dp){
+        if(n<=1)
+        return 1;
+        if(dp[n]!=-1)
+        return dp[n];
+        else
+        return dp[n]= fibo(n-1,dp)+fibo(n-2,dp);
+    }
+}
